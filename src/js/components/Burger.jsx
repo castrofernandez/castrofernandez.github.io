b7bf3ae8aa9409f7c84d169015ccb22bf8a3be95
Juan Castro Fernández <castrofernandez@gmail.com>
2020-01-31 21:22:13 +0100

    Adds styled-components for Burger

--- src/js/components/Burger.jsx
+++ src/js/components/Burger.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import GLOBALS from '../styles/globals';
+
+const BurgerLink = styled.a`
+    display: block;
+    padding: 15px 0 0 15px;
+`;
+
+const BurgerLine = styled.span`
+    display: block;
+    width: 36px;
+    height: 4px;
+    background-color: ${GLOBALS.colours.text.lighter};
+    margin-bottom: 5px;
+`;
 
 const Burger = () => {
     return (
-        <a href="#" className="burger">
-            <span />
-            <span />
-            <span />
-        </a>
+        <BurgerLink href="#">
+            <BurgerLine />
+            <BurgerLine />
+            <BurgerLine />
+        </BurgerLink>
     );
 };
 
---
+++ src/js/styles/globals.js
@@ -0,0 +1,7 @@
+export default {
+    colours: {
+        text: {
+            lighter: '#4a4a4a'
+        }
+    }
+};
