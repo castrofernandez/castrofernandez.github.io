6efde499be47daad3015a6c7dde49b1aa8a73c5e
Juan Castro Fernández <castrofernandez@gmail.com>
2020-02-01 11:12:58 +0100

    Adds styled-components to Clock

--- src/js/components/Clock.jsx
+++ src/js/components/Clock.jsx
@@ -1,4 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+import GLOBALS from '../styles/globals';
+
+const ClockWrapper = styled.p`
+    margin: -2px 0 0 0;
+    color: ${GLOBALS.colours.text.lighter};
+    height: 70px;
+
+    @media (max-width: ${GLOBALS.sizes.tablet}) {
+        height: 40px;
+    }
+
+    @media (max-width: ${GLOBALS.sizes.mobile}) {
+        display: none;
+    }
+`;
+
+const Time = styled.span`
+    font: 400 80px/70px ${GLOBALS.fonts.pixel};
+
+    @media (max-width: ${GLOBALS.sizes.tablet}) {
+        font-size: 30px;
+        line-height: 40px;
+    }
+`;
+
+const Seconds = styled.span`
+    font: 400 60px/70px ${GLOBALS.fonts.pixel};
+
+    @media (max-width: ${GLOBALS.sizes.tablet}) {
+        font-size: 22px;
+        line-height: 40px;
+    }
+`;
+
+const blink = keyframes`
+    50% {
+        opacity: 0;
+    }
+`;
+
+const Blinker = styled.span`
+    font: 400 80px/70px ${GLOBALS.fonts.pixel};
+    animation: ${blink} 1s ease infinite;
+
+    @media (max-width: ${GLOBALS.sizes.tablet}) {
+        font-size: 30px;
+        line-height: 40px;
+    }
+
+    &:before {
+        content: ':';
+    }
+`;
 
 const formatTime = (value) => value.length < 2 ? '0' + value : value;
 
@@ -14,12 +68,12 @@ const Clock = () => {
     }, []);
 
     return (
-        <p className="clock">
-            <span className="time hour">{formatTime(time.getHours().toString())}</span>
-            <span className="blinker" />
-            <span className="time minutes">{formatTime(time.getMinutes().toString())}</span>
-            <span className="seconds">{formatTime(time.getSeconds().toString())}</span>
-        </p>
+        <ClockWrapper>
+            <Time className="hour">{formatTime(time.getHours().toString())}</Time>
+            <Blinker />
+            <Time className="minutes">{formatTime(time.getMinutes().toString())}</Time>
+            <Seconds>{formatTime(time.getSeconds().toString())}</Seconds>
+        </ClockWrapper>
     );
 };
 
--- src/js/styles/globals.js
+++ src/js/styles/globals.js
@@ -1,7 +1,16 @@
 export default {
+    sizes: {
+        mobile: '767px',
+        tablet: '1023px',
+        tabletLandscape: '1024px',
+        smallDesktop: '1300px'
+    },
     colours: {
         text: {
             lighter: '#4a4a4a'
         }
+    },
+    fonts: {
+        pixel: 'pixel, sans-serif'
     }
 };
