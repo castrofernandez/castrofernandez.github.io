7801fed7e68d78c569dccbc96e236c9a3e4ebe5b
Juan Castro Fernández <castrofernandez@gmail.com>
2020-02-12 18:25:14 +0100

    Refactors translation files.

---
+++ src/js/containers/keys/ast.js
@@ -0,0 +1,64 @@
+const ast = {
+    intro: 'entamu',
+    experience: 'esperiencia',
+    studies: 'estudios',
+    projects: 'proyeutos',
+    welcome: '¡Bienveníu!',
+    computerScience: 'Informática',
+    frontender: 'Desenvolvedor frontend',
+    fullstack: 'Desenvolvedor fullstack',
+    designPatterns: 'Patrones de diseñu',
+    edreamsPosition: 'Desenvolvedor Full Stack',
+    edreamsDescription: 'Desenvuelvo web y caltenimientu de productu',
+    theCocktailPosition: 'Desenvolvedor Full Stack',
+    theCocktailDescription: "Desenvuelvo web y caltenimientu d'aplicaciones",
+    simbiosysPosition: 'Desenvolvedor Full Stack',
+    simbiosysDescription: 'Desenvuelvo web y xestión de proyectos',
+    wesoPosition: 'Desenvolvedor Full Stack',
+    wesoDescription: 'Desenvuelvo web y xestión de proyectos',
+    espiralPosition: 'Desenvolvedor Full Stack',
+    espiralDescription: 'Desenvuelvo web y caltenimientu de productu',
+    ingescoPosition: 'Desenvolvedor Backend',
+    ingescoDescription: "Desenvuelvo web y caltenimientu d'aplicaciones",
+    cetsPosition: 'Maestru',
+    cetsDescription: 'Profesor de clases particulares para universitarios',
+    programming: 'Introducción a la Programación',
+    dataStructures: 'Estructures de datos',
+    algorithms: 'Algoritmia',
+    master: "Máster n'Inxeniería Web",
+    compilers: 'Compiladores ya intérpretes',
+    degree: 'Inxeniería Informática de Sistemes',
+    operatingSystems: 'Sistemes Operativos',
+    networks: 'Redes',
+    dataBases: 'Bases de Datos',
+    softwareEngineering: 'Inxeniería del Software',
+    universityOviedo: "Universidá d'Uviéu",
+    ireland: 'Irlanda',
+    spanish: 'Español',
+    asturian: 'Asturianu',
+    english: 'Inglés',
+    french: 'Francés',
+    german: 'Alemán',
+    italian: 'Italianu',
+    native: 'Falante nativu',
+    basicKnowledge: 'Conocencies básiques',
+    eoi: "Escuela Oficial d'Idiomes",
+    title: 'títulu',
+    tasks: 'xeres',
+    wwwfTasks:
+        "Xestión del proyectu. Integración d'interfaces d'usuariu y desenvolvimientu d'arquitectura en WordPress.",
+    a4aiTasks:
+        'Responsable del proyeutu. Diseñu, implementación y puesta en funcionamientu.',
+    windexTasks:
+        'Responsable del proyeutu. Diseñu, implementación y puesta en funcionamientu.',
+    circoTasks:
+        'Responsable del proyeutu. Diseñu, implementación y puesta en funcionamientu.',
+    wescountryTasks:
+        'Responsable del proyeutu. Diseñu, implementación y puesta en funcionamientu.',
+    landportalTasks:
+        'Responsable del proyeutu. Diseñu, implementación y puesta en funcionamientu.',
+    wfTasks:
+        "Responsable d'interfaces d'usuariu. Diseñu, implementación y puesta en funcionamientu."
+};
+
+export default ast;
---
+++ src/js/containers/keys/es.js
@@ -0,0 +1,64 @@
+const es = {
+    intro: 'inicio',
+    experience: 'experiencia',
+    studies: 'estudios',
+    projects: 'proyectos',
+    welcome: '¡Bienvenido!',
+    computerScience: 'Informática',
+    frontender: 'Desarrollador frontend',
+    fullstack: 'Desarrollador fullstack',
+    designPatterns: 'Patrones de diseño',
+    edreamsPosition: 'Desarrollador Full Stack',
+    edreamsDescription: 'Desarrollo web y mantenimiento de producto',
+    theCocktailPosition: 'Desarrollador Full Stack',
+    theCocktailDescription: 'Desarrollo web y mantenimiento de aplicaciones',
+    simbiosysPosition: 'Desarrollador Full Stack',
+    simbiosysDescription: 'Desarrollo web y gestión de proyectos',
+    wesoPosition: 'Desarrollador Full Stack',
+    wesoDescription: 'Desarrollo web y gestión de proyectos',
+    espiralPosition: 'Desarrollador Full Stack',
+    espiralDescription: 'Desarrollo web y mantenimiento de producto',
+    ingescoPosition: 'Desarrollador Backend',
+    ingescoDescription: 'Desarrollo web y mantenimiento de aplicaciones',
+    cetsPosition: 'Profesor',
+    cetsDescription: 'Profesor de clases particulares para universitarios',
+    programming: 'Introducción a la Programación',
+    dataStructures: 'Estructuras de datos',
+    algorithms: 'Algoritmia',
+    master: 'Máster en Ingeniería Web',
+    compilers: 'Compiladores e intérpretes',
+    degree: 'Ingeniería Informática de Sistemas',
+    operatingSystems: 'Sistemas Operativos',
+    networks: 'Redes',
+    dataBases: 'Bases de Datos',
+    softwareEngineering: 'Ingeniería del Software',
+    universityOviedo: 'Universidad de Oviedo',
+    ireland: 'Irlanda',
+    spanish: 'Español',
+    asturian: 'Asturiano',
+    english: 'Inglés',
+    french: 'Francés',
+    german: 'Alemán',
+    italian: 'Italiano',
+    native: 'Hablante nativo',
+    basicKnowledge: 'Conocimientos básicos',
+    eoi: 'Escuela Oficial de Idiomas',
+    title: 'título',
+    tasks: 'tareas',
+    wwwfTasks:
+        'Gestión del proyecto. Integración de interfaces de usuario y desarrollo de arquitectura en WordPress.',
+    a4aiTasks:
+        'Responsable del proyecto. Diseño, implementación y puesta en funcionamiento.',
+    windexTasks:
+        'Responsable del proyecto. Diseño, implementación y puesta en funcionamiento.',
+    circoTasks:
+        'Responsable del proyecto. Diseño, implementación y puesta en funcionamiento.',
+    wescountryTasks:
+        'Responsable del proyecto. Diseño, implementación y puesta en funcionamiento.',
+    landportalTasks:
+        'Responsable del proyecto. Diseño, implementación y puesta en funcionamiento.',
+    wfTasks:
+        'Responsable de interfaces de usuario. Diseño, implementación y puesta en funcionamiento.'
+};
+
+export default es;
